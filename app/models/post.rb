8c474c236689bc4b054e99fa3142537124b7542d
marmite334 <68127550+marmite334@users.noreply.github.com>
2021-01-28 15:37:12 +0900

    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :content
+    validates :category_id, numericality: { other_than: 1, message: 'Select' }
+  end
   belongs_to :user
   has_one_attached :image
 end
