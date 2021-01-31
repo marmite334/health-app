d21af8592acdef364879581e84f7c2f82abfc477
marmite334 <68127550+marmite334@users.noreply.github.com>
2021-01-31 21:17:27 +0900

    アソシエーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
     validates :category_id, numericality: { other_than: 1, message: 'Select' }
   end
   belongs_to :user
+  has_many :comments
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to       :category
