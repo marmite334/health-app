05dea54babc86247f4cc1d5e70a1e1da1bc0b88e
marmite334 <68127550+marmite334@users.noreply.github.com>
2021-02-02 10:36:54 +0900

    userのfactoryの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Name.last_name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1)}
+    password_confirmation {password}
+    birth_date            { '1996-07-18' }
   end
 end
