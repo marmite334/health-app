db78046fe976722330fa6b4e6438b5e2b90bb955
marmite334 <68127550+marmite334@users.noreply.github.com>
2021-02-18 22:39:23 +0900

    テストコードの記述

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :chat do
-    
+    text {Faker::String.random}
+    created_at {Time.now}
+    updated_at {Time.now}
+    association :user
   end
 end
--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @chat = FactoryBot.build(:chat)
+  end
+
+  describe 'コメントの投稿' do
+
+    context 'コメントの投稿に成功する場合' do
+
+      it '必要な情報が正しく入力されていれば投稿に成功する' do
+        expect(@chat).to be_valid
+      end
+      
+    end
+
+    context 'コメントの投稿に失敗する場合' do
+
+      it 'textが空では投稿に失敗する' do
+        @chat.text = nil
+        @chat.valid?
+        expect(@chat.errors.full_messages).to include("Text can't be blank")
+      end
+
+      it '紐ずくユーザーが存在しないと投稿に失敗する' do
+        @chat.user_id = nil
+        @chat.valid?
+        expect(@chat.errors.full_messages).to include("User must exist")
+      end
+
+    end
+
+  end
 end
