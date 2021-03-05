cc21c858d92f0cc4116d2227232aa479293c7399
marmite334 <68127550+marmite334@users.noreply.github.com>
2021-03-05 20:56:19 +0900

    アクティブハッシュの設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,19 +1,20 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '（仮）1' },
-    { id: 3, name: '（仮）2' },
-    { id: 4, name: '（仮）3' },
-    { id: 5, name: '（仮）4' },
-    { id: 6, name: '（仮）5' },
-    { id: 7, name: '（仮）6' },
-    { id: 8, name: '（仮）7' },
-    { id: 9, name: '（仮）8' },
-    { id: 10, name: '（仮）9' },
-    { id: 11, name: 'その他' }
+    { id: 2, name: '免疫' },
+    { id: 3, name: '栄養' },
+    { id: 4, name: '食育' },
+    { id: 5, name: '健康食品' },
+    { id: 6, name: '医療' },
+    { id: 7, name: '介護' },
+    { id: 8, name: '福祉' },
+    { id: 9, name: '衛生' },
+    { id: 10, name: '分子生物学' },
+    { id: 11, name: '病理学' },
+    { id: 12, name: '感性科学' },
+    { id: 13, name: 'その他' }
   ]
 
   include ActiveHash::Associations
   has_many :post
-  belongs_to :chat
 end
